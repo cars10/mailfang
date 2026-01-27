e60e7686c32719968bdc3dadfae0949a908983d9
Carsten König <carstenkoenig92@gmail.com>
2026-01-27 19:38:15 +0100

    adjust search help

--- frontend/src/components/EmailLayout/EmailList.vue
+++ frontend/src/components/EmailLayout/EmailList.vue
@@ -18,7 +18,7 @@
           />
           <button
             type="button"
-            class="absolute right-2 top-1/2 -translate-y-1/2 text-xs text-gray-500 hover:text-gray-700 focus:outline-none underline cursor-pointer"
+            class="absolute right-2 top-1/2 -translate-y-1/2 text-xs text-gray-500 hover:text-gray-700 focus:outline-none hover:underline cursor-pointer"
             @click="showHelpModal = true"
           >
             help
