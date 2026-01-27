ed3628615da759945ccb2283a151236be48b6941
Carsten König <carstenkoenig92@gmail.com>
2026-01-27 22:00:27 +0100

    sort email headers

--- frontend/src/components/EmailContent/EmailSmtpHeaders.vue
+++ frontend/src/components/EmailContent/EmailSmtpHeaders.vue
@@ -2,7 +2,7 @@
   <div class="p-4 grow">
     <table>
       <tbody>
-        <template v-for="(values, key) in email.headers" :key="key">
+        <template v-for="[key, values] in sortedHeaders" :key="key">
           <tr>
             <td class="text-gray-700 select-text px-2 text-nowrap">
               {{ key }}:
@@ -18,8 +18,15 @@
 </template>
 
 <script setup lang="ts">
+  import { computed } from 'vue'
   import type { EmailRecord } from '@/types/email'
   import CopyBadge from '@/components/shared/CopyBadge/CopyBadge.vue'
 
-  defineProps<{ email: EmailRecord }>()
+  const props = defineProps<{ email: EmailRecord }>()
+
+  const sortedHeaders = computed(() => {
+    return Object.entries(props.email.headers).sort(([a], [b]) =>
+      a.toLowerCase().localeCompare(b.toLowerCase())
+    )
+  })
 </script>
